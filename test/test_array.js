d780df9c81773a9d25e75777e3efc2026bd35e7a
yuki kurihara <co000ri@gmail.com>
2012-05-05 03:28:01 +0900

    Arrayはやっぱprototypeに

--- test/test_array.js
+++ test/test_array.js
@@ -7,37 +7,40 @@ var testCase = [
 
 function newTest () {
 	assert.strictEqual(nil, null, 'can use nil same null');
-	assert.deepEqual(Array.new().to_a(), new Array());
-	assert.deepEqual(Array.new(3).to_a(), [nil, nil, nil]);
-	assert.deepEqual(Array.new(3, {"foo":5}).to_a(), [{"foo":5},{"foo":5},{"foo":5}]);
+	assert.deepEqual(Array.new(), new Array());
+	assert.deepEqual(Array.new(3), [nil, nil, nil]);
+	assert.deepEqual(Array.new([1,2,3]), [1,2,3]);
+	assert.deepEqual(Array.new(3, {"foo":5}), [{"foo":5},{"foo":5},{"foo":5}]);
 	assert.throws(function(){
 		Array.new("foo")
 	});
 },
 
 function pushTest () {
-	var array = Array.new();
-	array.push(1).push(3).push(5);
-	assert.deepEqual(array.to_a(), [1,3,5]);
+	var array = [0].push(1).push(3);
+	array._push(5);
+	assert.deepEqual(array, [0,1,3,5]);
 },
 
 function popTest () {
-	var array = Array.new([1,2,3,4,5,6,7]);
+	var array = [1,2,3,4,5,6,7,8,9];
+	assert.equal(array.pop(2)._pop(), 9);
+	assert.deepEqual(array, [1,2,3,4,5,6,7]);
 	assert.equal(array.pop(), 7);
-	assert.deepEqual(array.to_a(), [1,2,3,4,5,6]);
+	assert.deepEqual(array, [1,2,3,4,5,6]);
 	assert.deepEqual(array.pop(0), []);
-	assert.deepEqual(array.to_a(), [1,2,3,4,5,6]);
+	assert.deepEqual(array, [1,2,3,4,5,6]);
 	assert.deepEqual(array.pop(1), [6]);
-	assert.deepEqual(array.to_a(), [1,2,3,4,5]);
+	assert.deepEqual(array, [1,2,3,4,5]);
 	assert.deepEqual(array.pop(3), [3,4,5]);
 	assert.deepEqual(array.pop(10), [1,2]);
-	assert.deepEqual(array.to_a(), []);
+	assert.deepEqual(array, []);
 },
 
 function eachTest () {
-	var i, ret
+	var i, ret, array;
 	i = 0;
-	array = Array.new([1,3,5]);
+	array = [1,3,5];
 	ret = array.each(function (i) {
 		assert.equal(typeof i, 'number');
 		assert.ok(0 < i);
@@ -45,7 +48,7 @@ function eachTest () {
 		i = i + 5;
 	});
 	assert.equal(i, 0, 'keep value');
-	assert.deepEqual(ret, array.to_a());
+	assert.deepEqual(ret, array);
 	assert.throws(function(){
 		[]._each(function(){});
 	});
