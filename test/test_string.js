3185bce113019a3c0c34cb76c441ad14da855715
yuki kurihara <co000ri@gmail.com>
2012-05-13 22:23:24 +0900

    mv files

--- test/test_string.js
+++ test/test_string.js
@@ -20,13 +20,27 @@ function newTest () {
 	assert.throws(function(){ String.new([1,2,3]) });
 },
 
+function subTest () {
+	assert.strictEqual('abcdefg'.sub("def", '!!'), "abc!!g");
+	assert.strictEqual('abcdefg'.sub(/def/, '!!'), "abc!!g");
+	assert.strictEqual('abcabc'.sub(/b/, '<<$&>>'), "a<<b>>cabc"); // RegExp use JavaScript
+	assert.strictEqual('xbbb-xbbb'.sub(/x(b+)/, '$1'), "bbb-xbbb");
+	assert.notStrictEqual('xbbb-xbbb'.sub(/x+(b+)/, '$$1'), "bbb-xbbb");
+	assert.strictEqual("abcabc".sub(/[ab]/, {"a": "!", "1": "#"}), "!bcabc");
+	assert.strictEqual("abcabc".sub(/[bc]/, {'b':'B', 'c':'C'}), "aBcabc");
+	assert.strictEqual("abcabc".sub(/b/, Hash.new({'b':'B', 'c':'C'})), "aBcabc");
+},
+
 function gsubTest () {
 	assert.strictEqual('abcdefg'.gsub("def", '!!'), "abc!!g");
 	assert.strictEqual('abcdefg'.gsub(/def/, '!!'), "abc!!g");
-	assert.strictEqual('abcabc'.gsub(/b/, '<<$&>>'), "a<<b>>ca<<b>>c"); // RegExp use JavaScript
+	assert.strictEqual('abcabc'.gsub(/b/, '<<$&>>'), "a<<b>>ca<<b>>c");
 	assert.strictEqual('xxbbxbb'.gsub(/x+(b+)/, 'X<<$1>>'), "X<<bb>>X<<bb>>");
 	assert.strictEqual('xbbb-xbbb'.gsub(/x(b+)/, '$1'), "bbb-bbb");
 	assert.notStrictEqual('xbbb-xbbb'.gsub(/x(b+)/, '$$1'), "bbb-bbb");
+	assert.strictEqual("abcabc".gsub(/[ab]/, {"a": "!", "1": "#"}), "!c!c");
+	assert.strictEqual("abcabc".gsub(/[bc]/, {'b':'B', 'c':'C'}), "aBCaBC");
+	assert.strictEqual("abcabc".gsub(/[bc]/, Hash.new({'b':'B', 'c':'C'})), "aBCaBC");
 },
 
 ];
