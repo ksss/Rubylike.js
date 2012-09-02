0aa9cf033d1e230faf01c075ee859050cae7d6bc
yuki kurihara <co000ri@gmail.com>
2012-09-03 00:16:32 +0900

    add array function

--- test/test_array.js
+++ test/test_array.js
@@ -23,6 +23,69 @@ function newTest () {
 	});
 },
 
+function assocTest () {
+	var array = [[1,15], [2,25], ["foo",35], [{"a":3},3]];
+	assert.deepEqual(array.assoc(2), [2,25]);
+	assert.strictEqual(array.assoc(100), nil);
+	assert.strictEqual(array.assoc(15), nil);
+	assert.deepEqual(array.assoc("foo"), ["foo",35]);
+	assert.deepEqual(array.assoc({"a":3}), [{"a":3},3]);
+},
+
+function atTest () {
+	var array = [0,1,2,"foo"];
+	assert.strictEqual(array.at(1), 1);
+	assert.strictEqual(array.at(3), "foo");
+	assert.strictEqual(array.at(10), nil);
+	assert.strictEqual(array.at("at"), nil);
+},
+
+function clearTest () {
+	var array = [1,2,3];
+	var array_copy = array;
+	assert.strictEqual(array.clear(), array_copy);
+	assert.strictEqual(array, array_copy);
+	assert.deepEqual(array, []);
+},
+
+function cloneTest () {
+	var array = [{"foo":1}];
+	var copy = array.clone();
+	assert.ok(array !== copy);
+	assert.ok(array[0] === copy[0]);
+	array = [1,2,3];
+},
+
+function collectTest () {
+	var array = [10,20,30];
+	var collect = array.collect(function(i){return i * 10});
+	var enumerator = array.collect();
+	var tmp = [];
+	enumerator.each(function(i){
+		tmp.push(i + 2);
+	});
+	assert.deepEqual(collect, [100,200,300]);
+	assert.deepEqual(tmp, [12,22,32]);
+},
+
+function eachTest () {
+	var i = 0;
+	var array = [1,3,5];
+	var enumerator = array.each();
+	var ret = array.each(function (i) {
+		assert.equal(typeof i, 'number');
+		assert.ok(0 < i);
+		assert.ok(i < 6);
+		i = i + 5;
+	});
+	assert.equal(i, 0, 'keep value');
+	assert.deepEqual(ret, array);
+	assert.deepEqual(enumerator, [1,3,5]);
+	assert.throws(function(){
+		[]._each(function(){});
+	});
+},
+
 function pushTest () {
 	var array = [0].push(1).push(3);
 	array._push(5);
@@ -57,23 +120,6 @@ function shiftTest () {
 	assert.strictEqual(array.shift(), null);
 	assert.deepEqual(array.shift(1), []);
 	assert.deepEqual(array, []);
-},
-
-function eachTest () {
-	var i, ret, array;
-	i = 0;
-	array = [1,3,5];
-	ret = array.each(function (i) {
-		assert.equal(typeof i, 'number');
-		assert.ok(0 < i);
-		assert.ok(i < 6);
-		i = i + 5;
-	});
-	assert.equal(i, 0, 'keep value');
-	assert.deepEqual(ret, array);
-	assert.throws(function(){
-		[]._each(function(){});
-	});
 },
 
 function firstTest () {
